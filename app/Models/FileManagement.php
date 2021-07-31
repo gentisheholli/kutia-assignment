0b7c2cc7abef410b955528852ec921fd87d3f69d
gentisheholli <gentisheholli@gmail.com>
2021-07-31 10:59:08 +0200

    Reverted last change and added relationships

--- app/Models/FileManagement.php
+++ app/Models/FileManagement.php
@@ -23,4 +23,10 @@ class FileManagement extends Model
      *
      * @var array
      */
+
+    public function user()
+    {
+      return $this->belongsTo('App\Models\User','users');
+    }
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password'
     ];
 
     /**
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function files()
+    {
+      return $this->hasMany('App\Models\FileManagement','fileManagement');
+    }
 }
